d480ee82237fcd036f494bae036a64fd83cbb336
Andrew Joia <drewdrewthis@gmail.com>
2015-11-11 10:25:49 -0500

    Adds voterinfo AJAX and container

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(function() {
 
 	// Outputs returned data
-    function printData(data) {
+    function printReps(data) {
 		console.log(data);
 
         // Gets and outputs info for each official by office 
@@ -33,6 +33,40 @@ $(function() {
 		});
 	};
 
+    // Outputs returned data
+    function printVoterInfo(data) {
+        console.log(data);
+
+        /*
+        $.each(data.offices, function(key, office){
+            var index = office.officialIndices[0],
+                official = data.officials[index],
+                context = {
+                    'office': office,
+                    'official':official
+                },
+                // Handlebars compiler
+                source = $("#template-1").html(),
+                template = Handlebars.compile(source),
+                html = template(context);
+            
+            if(office.levels) {
+                switch(office.levels[0]) {
+                    case "country":
+                        $('.national').append(html);
+                        break;
+                    default:
+                        $('.state').append(html);
+                        break;
+                };
+            }
+            else {
+                $('.city').append(html);
+            }
+        });
+        */
+    };
+
     // Main submit
     $('#address_lookup').submit(function(e){
     	e.preventDefault();
@@ -40,7 +74,8 @@ $(function() {
     	var input = $('input').val(),
     		params = {
 				address: input,
-				key: "AIzaSyB73MDWfXrB7DjxUlfN3vPUGmHbF_l5wUQ"
+				key: "AIzaSyB73MDWfXrB7DjxUlfN3vPUGmHbF_l5wUQ",
+                electionId: 2000
 			};
 
         // Makes an AJAX request with user supplied address and returns civic data
@@ -51,13 +86,30 @@ $(function() {
     		data: params
     	})
     	.done(function(data) {
-    		console.log('AJAX Request Succeeded');
-    		printData(data);
-    		$('input[name="user-input"]').val("");
+    		console.log('Reps Returned');
+    		printReps(data);
     	})
     	.fail(function() {
-    		console.log('AJAX Request Failed');
-    		$('.address-output').append("Invalid Entry");
+    		console.log('AJAX Reps Request Failed');
+    		$('.voter-info').append("Invalid Entry");
     	});
+
+         // Makes an AJAX request with user supplied address and returns voter info data
+        $.ajax({
+            method: "GET",
+            url: "https://www.googleapis.com/civicinfo/v2/voterinfo",
+            dataType: "json",
+            data: params
+        })
+        .done(function(data) {
+            console.log('VoterInfo Succeeded');
+            printVoterInfo(data);
+        })
+        .fail(function() {
+            console.log('AJAX VoterInfo Request Failed');
+            $('.voter-info').append("Invalid Entry");
+        });
+
+        $('input[name="user-input"]').val(""); 
     });
 });
