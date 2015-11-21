bc508f4d8cf24670612fd3d494f4680c517a5abc
Andrew Joia <drewdrewthis@gmail.com>
2015-11-21 15:49:29 -0500

    Gets Twitter timeline to load for each politician box.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+var twitFunc = function() {
+    !function(d,s,id){
+        var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
+        if(!d.getElementById(id)){
+            js=d.createElement(s);
+            js.id=id;
+            js.src=p+"://platform.twitter.com/widgets.js";
+            fjs.parentNode.insertBefore(js,fjs);
+        }
+    }(document,"script","twitter-wjs");
+};
+
 $(function() {
 
 	// Outputs returned data
@@ -16,6 +28,8 @@ $(function() {
                 source = $("#template-1").html(),
                 template = Handlebars.compile(source),
                 html = template(context);
+
+                
             
             if(office.levels) {
                 switch(office.levels[0]) {
@@ -31,7 +45,12 @@ $(function() {
                 $('.city').append(html);
             }
 		});
+
+        twitFunc();
 	};
+
+    
+
 
     // Outputs returned data
     function printVoterInfo(data) {
